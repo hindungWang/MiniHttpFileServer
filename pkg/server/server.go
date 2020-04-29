9e1f87baf56284fb6c2020bc69c657aaf6826263
hxd <836778763@qq.com>
2020-04-29 20:42:34 +0800

    修改gitignore

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -2,16 +2,16 @@ package server
 
 import (
 	"fmt"
-	"github.com/vinkdong/gox/log"
+	"log"
 	"net/http"
 )
 
-type  Server struct {
+type Server struct {
 	Path string
 	Port string
 }
 
-func (s *Server)Start() error {
+func (s *Server) Start() error {
 	http.Handle("/", http.FileServer(http.Dir(s.Path)))
 	log.Infof("server starting on %s,exposed path at %s...", s.Port, s.Path)
 	err := http.ListenAndServe(fmt.Sprint(":", s.Port), nil)
